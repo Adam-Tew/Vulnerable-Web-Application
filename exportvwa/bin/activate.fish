d89123103ec5b36034b294ca3bf61526d6d2584c
FjolsvinAvJotenheimr <40150847+FjolsvinAvJotenheimr@users.noreply.github.com>
2025-01-27 15:10:21 +0100

    Delete exportvwa directory
    Make place for new version

--- exportvwa/bin/activate.fish
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e7bd263216bea78ac62e0a701e1a40b3dc29de83fa9a04ae4b53c7bfcdc3e5f4
-size 2197
--- exportvwa/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:36b5d189c09437969f1f41b0008319904ea57746aa92c24e252f94ddc093cc03
-size 44181
